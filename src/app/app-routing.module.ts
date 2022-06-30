51188e624d2dcdfbb7c127d0881dd10f4c07450a
EresseaNui <leteneur.roland@hotmail.fr>
2022-06-30 13:42:38 +0200

    add router path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { NotFoundComponent } from './_pages/not-found/not-found.component';
+import { ProductsPageComponent } from './_pages/products-page/products-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './_pages/home-page/home-page.component';
 
-const routes: Routes = [{ path: 'home', component: HomePageComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: 'product', component: ProductsPageComponent },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
