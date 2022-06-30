fba5d4d1ba383b9d21e5fbf01ed8a80e74515faa
EresseaNui <leteneur.roland@hotmail.fr>
2022-06-30 14:05:40 +0200

    update link

--- src/app/_commons/navbar/navbar.component.ts
+++ src/app/_commons/navbar/navbar.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  navlinks = [
+    { id: 1, name: 'Produits', uri: '/product' },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
